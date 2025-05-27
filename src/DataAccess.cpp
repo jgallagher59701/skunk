fcf6c1171881814e10bbaaee06b935dacfc8b9a4
James Gallagher <jgallagher@opendap.org>
2025-05-27 14:18:48 -0600

    Added some more stuff; DataAccess.

---
+++ src/DataAccess.cpp
@@ -0,0 +1,5 @@
+//
+// Created by James Gallagher on 5/27/25.
+//
+
+#include "DataAccess.h"
---
+++ src/DataAccess.h
@@ -0,0 +1,16 @@
+//
+// Created by James Gallagher on 5/27/25.
+//
+
+#ifndef DATAACCESS_H
+#define DATAACCESS_H
+
+
+
+class DataAccess {
+
+};
+
+
+
+#endif //DATAACCESS_H
