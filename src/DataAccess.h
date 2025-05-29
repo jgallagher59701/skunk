a5437e879381c33132e9fd13fdca16a9db10fd9c
James Gallagher <jgallagher@opendap.org>
2025-05-28 22:02:51 -0600

    Added DataAccess

--- src/DataAccess.h
+++ src/DataAccess.h
@@ -5,9 +5,31 @@
 #ifndef DATAACCESS_H
 #define DATAACCESS_H
 
+#include <string>
+#include <memory>
+
+#include "MemoryCache.h"
+
+namespace libdap {
+class DMR;
+
+}
 
 
 class DataAccess {
+    /// Add an instance of MemoryCache here.
+    MemoryCache &cache_;
+
+public:
+    DataAccess() = delete;
+    explicit DataAccess(MemoryCache &cache) cache_(cache) { }
+    DataAccess(const DataAccess&) = delete;
+    DataAccess(DataAccess&&) noexcept = delete;
+    DataAccess& operator=(const DataAccess&) = delete;
+    DataAccess& operator=(DataAccess&&) noexcept = delete;
+    virtual ~DataAccess() = default;
+
+    virtual std::unique_ptr<DMR> getDMR(const std::string &path, const std::string &ce, const std::string &func) = 0;
 
 };
 
