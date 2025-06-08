7080ca96975e143d4d666f8215c282f47f124dca
James Gallagher <jgallagher@opendap.org>
2025-06-07 23:28:13 -0600

    Simple path parsing

--- services/opendap/src/opendap_service.cpp
+++ services/opendap/src/opendap_service.cpp
@@ -28,6 +28,15 @@ std::string format_extension(const std::string& filename) {
     return filename.substr(last_dot_pos);
 }
 
+#if 0
+// test strings for the above
+std::string filename1 = "example.txt";
+  std::string filename2 = "archive.tar.gz";
+  std::string filename3 = "no_extension";
+  std::string filename4 = "path/to/file.name.ext";
+  std::string filename5 = "path.with.dots/file";
+#endif
+
 enum data_format {
     nc,
     unknown
@@ -40,10 +49,12 @@ enum data_format {
  */
 data_format find_format(const string &data_path) {
     auto format = format_extension(data_path);
-    if (format == "nc")
+    if (format == ".nc")
         return nc;
     return unknown;
 }
+
+#if 0
 
 unique_ptr<DataAccess>  find_data_access(const enum data_format &df) {
     switch (df) {
@@ -54,25 +65,44 @@ unique_ptr<DataAccess>  find_data_access(const enum data_format &df) {
     }
 }
 
+#endif
+
 int main() {
     httplib::Server svr;
 
     // Register loger
     // Register exception handler
 
-    svr.Get(R"(/(?<data_path>\.+).dmr)", [](const httplib::Request& req, httplib::Response& res) {
+    // The DMR return
+    svr.Get(R"(/(.+)\.dmr)", [](const httplib::Request& req, httplib::Response& res) {
         // Extract parameters
-        const auto data_path = req.path_params.at("data_path");
+        if (req.matches.empty()) {
+            res.set_content("Error - no data path found", "text/plain");
+        }
+        const auto data_path = req.matches[1];
         const auto ce = req.has_param("dap4_ce") ? req.get_param_value("dap4_ce") : "";
         const auto function = req.has_param("dap4_function") ? req.get_param_value("dap4_function") : "";
+
         // Determine data format
         const auto format = find_format(data_path);
+
+        // Output
+        ostringstream oss;
+        oss << "data_path: " << data_path << '\n';
+        oss << "dap4_ce: " << ce << '\n';
+        oss << "dap4_function: " << function << '\n';
+
+        if (format == nc)
+            oss << "format: nc\n";
+        else
+            oss << "format: unknown\n";
+
         // Get the DataAccess instance
-        DataAccess da = find_data_access(format);
+        // DataAccess da = find_data_access(format);
         // Get the DMR
         // Build the DMR response
         // Return the DMR response
-        res.set_content(handle_request(req), "text/plain");
+        res.set_content(oss.str(), "text/plain");
     });
 
     svr.listen("localhost", 8080);
