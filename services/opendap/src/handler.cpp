3a337a3231204a5d738e02fb5ca172bbdd651364
James Gallagher <jgallagher@opendap.org>
2025-06-16 15:44:49 -0600

    Works. Build hacked - Blunt force trauma - for CLion

--- services/opendap/src/handler.cpp
+++ services/opendap/src/handler.cpp
@@ -11,15 +11,15 @@ using namespace std;
 
 string get_extension(const string& filename) {
     // Find the last path separator
-    size_t last_slash_pos = filename.find_last_of('/');
+    const size_t last_slash_pos = filename.find_last_of('/');
 
     // Isolate the last path component (the file name)
-    string basename = (last_slash_pos != string::npos)
+    const string basename = (last_slash_pos != string::npos)
                       ? filename.substr(last_slash_pos + 1)
                       : filename;
 
     // Look for the last dot in the basename
-    size_t last_dot_pos = basename.rfind('.');
+    const size_t last_dot_pos = basename.rfind('.');
     if (last_dot_pos == string::npos) {
         // No extension in the filename part → return original input
         return filename;
@@ -28,24 +28,6 @@ string get_extension(const string& filename) {
     // Otherwise, return the extension
     return basename.substr(last_dot_pos);
 }
-
-#if 0
-/**
- * @brief Assume responses are determined by extension
- * Use an extension on the path to preserve compatibility with existing clients.
- * @param data_path
- * @return dap_response enum
- */
-dap_response find_response(const string &data_path) {
-    auto response = get_extension(data_path);
-    if (response == ".dmr")
-        return dmr;
-    else if (response == ".dap")
-        return dap;
-
-    return unknown_response;
-}
-#endif
 
 /**
  * @brief Assume formats are determined by extension.
@@ -60,7 +42,6 @@ data_format find_format(const string &data_path) {
 }
 
 #if 0
-
 unique_ptr<DataAccess>  find_data_access(const enum data_format &df) {
     switch (df) {
         case nc:
@@ -69,7 +50,6 @@ unique_ptr<DataAccess>  find_data_access(const enum data_format &df) {
             throw std::runtime_error("Unknown data format."0;)
     }
 }
-
 #endif
 
 void handle_dmr_request(const string &data_path, const httplib::Request& req, httplib::Response& res) {
@@ -80,11 +60,11 @@ void handle_dmr_request(const string &data_path, const httplib::Request& req, ht
     const auto format = find_format(data_path);
 
     if (format == nc) {
-        res.set_content("Error: only netCDF files can be served.", "text/plain");
-        return;
-    }
+    	// send response headers, blank line
+    	// send response body
+    	res.set_content("Moof!", "text/plain");
+		return;
+	}
 
-    // send response headers, blank line
-    // send response body
-    res.set_content("Moof!", "text/plain");
+    res.set_content("Error: only netCDF files can be served.", "text/plain");
 }
